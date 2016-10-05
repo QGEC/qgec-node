96f36c70f25efc22671b75be229b1cab6659047a
Peter <peterju35@gmail.com>
2016-10-05 16:32:27 -0400

    Add Delegates Section to home page, modularize some components

--- public/js/carousel_slide.js
+++ public/js/carousel_slide.js
@@ -1,3 +1,11 @@
-$('.carousel').carousel({
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  $('#head-carousel').carousel({
     interval: 5000 //changes the speed
-})
+  })
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 5500,
+  });
+});
